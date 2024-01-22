98e38720b3a403d46bbb052579c71d187b466cd9
Prakhar Doneria <gamesprotec@gmail.com>
2024-01-22 16:14:38 +0530

    Smooth Scroll

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 function copyCode() {
     var codeSnippet = document.getElementById('codeSnippet');
     
-    
     var range = document.createRange();
     range.selectNode(codeSnippet);
     window.getSelection().removeAllRanges();
     window.getSelection().addRange(range);
 
-    
     document.execCommand('copy');
 
-    
     window.getSelection().removeAllRanges();
 
-    
     alert('Code copied to clipboard!');
+}
+
+// Smooth scroll function
+function scrollToSection(sectionId) {
+    var targetSection = document.getElementById(sectionId);
+    
+    if (targetSection) {
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
 }
